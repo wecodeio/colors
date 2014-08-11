b84645b2c34e73e1a716d681008188c7fcdb6473
Lucas Videla <videla.lucas@gmail.com>
2014-08-10 23:26:56 -0300

    Refactor of the DSL

--- spec/colors_spec.js
+++ spec/colors_spec.js
@@ -1,29 +1,47 @@
 describe("When color created", function(){
   it("formats in RGB", function(){
-    expect(new color("#ff0000").rgb()).toEqual([255, 0, 0]);
+    expect(color("#ff0000").rgb()).toEqual([255, 0, 0]);
   });
   it("formats in HSL", function(){
-    expect(new color("#ff0000").hsl()).toEqual([0, 1, 0.5]);
+    expect(color("#ff0000").hsl()).toEqual([0, 1, 0.5]);
   });
   it("formats in css", function(){
-    expect(new color("#ff0000").css()).toEqual("#ff0000");
+    expect(color("#ff0000").css()).toEqual("#ff0000");
   });
 });
 
 describe("When color is lighten", function(){
   it("by zero remains equal", function(){
-    expect(new color("#ff0000").lighten(0).css()).toEqual("#ff0000");
+    expect(color("#ff0000").lighten(0).css()).toEqual("#ff0000");
   });
   it("by ten percent", function(){
-    expect(new color("#ff0000").lighten(.1).css()).toEqual("#ff1a1a");
+    expect(color("#ff0000").lighten(.1).css()).toEqual("#ff1a1a");
   });
   it("by fifty percent", function(){
-    expect(new color("#ff0000").lighten(.5).css()).toEqual("#ff8080");
+    expect(color("#ff0000").lighten(.5).css()).toEqual("#ff8080");
   });
   it("by ninety percent", function(){
-    expect(new color("#ff0000").lighten(.9).css()).toEqual("#ffe5e5");
+    expect(color("#ff0000").lighten(.9).css()).toEqual("#ffe5e5");
   });
   it("by hundred percent", function(){
-    expect(new color("#ff0000").lighten(1).css()).toEqual("#ffffff");
+    expect(color("#ff0000").lighten(1).css()).toEqual("#ffffff");
+  });
+});
+
+describe("When color is darken", function(){
+  it("by zero remains equal", function(){
+    expect(color("#ff0000").darken(0).css()).toEqual("#ff0000");
+  });
+  it("by ten percent", function(){
+    expect(color("#ff0000").darken(.1).css()).toEqual("#e60000");
+  });
+  it("by fifty percent", function(){
+    expect(color("#ff0000").darken(.5).css()).toEqual("#800000");
+  });
+  it("by ninety percent", function(){
+    expect(color("#ff0000").darken(.9).css()).toEqual("#190000");
+  });
+  it("by hundred percent", function(){
+    expect(color("#ff0000").darken(1).css()).toEqual("#000000");
   });
 });
